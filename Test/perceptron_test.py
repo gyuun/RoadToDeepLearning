08acd879e4cc701c967474dd9845a1df3ba10422
gyuun <csegyuun@pusan.ac.kr>
2024-08-12 17:57:35 +0900

    modify perceptron_test.py R0903 pylint error

--- Test/perceptron_test.py
+++ Test/perceptron_test.py
@@ -7,8 +7,14 @@ from Perceptron import perceptron
 
 class TestClass:
     """테스트 클래스에는 test prefix 가 존재해야함"""
+    def __init__(self, x):
+        """x = 1dim ndarray"""
+        self.p = perceptron.PerceptronLogic(x)
+
     def test_and(self):
         """테스트 함수에는 test_ prefix 가 존재해야함 """
-        x = np.array([0,0])
-        p = perceptron.PerceptronLogic(x)
-        assert p.and_gate() == 0
+        assert self.p.and_gate() == 0
+
+    def test_or(self):
+        """or게이트 테스트"""
+        assert self.p.and_gate() == 0
