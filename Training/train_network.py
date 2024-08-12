b15c87412fc910167dd81f962880e6b90ef85080
gyuun <csegyuun@pusan.ac.kr>
2024-08-12 18:43:18 +0900

    modify train_network.py for C0103

--- Training/train_network.py
+++ Training/train_network.py
@@ -14,22 +14,22 @@ if __name__ == '__main__':
     """
     train_loss_list = []
 
-    repetition = 10000
+    REPETITION = 10000
     train_size = x_train.shape[0]
-    batch_size = 100
+    BATCHSIZE = 100
 
-    learning_rate = 0.1
+    LEARNINGRATE = 0.1
     network = TwoLayerNet(input_size=784, hidden_size=50, output_size = 10)
 
-    for i in range(repetition):
-        batch_mask = np.random.choice(train_size, batch_size)
+    for i in range(REPETITION):
+        batch_mask = np.random.choice(train_size, BATCHSIZE)
         x_batch = x_train[batch_mask]
         t_batch = t_train[batch_mask]
 
         grad = network.numerical_gradient(x_batch, t_batch) # x_batch (100, 784) t_batch (100, 10)
 
         for key in ('W1', 'b1', 'W2', 'b2'):
-            network.params[key] -= learning_rate * grad[key]
+            network.params[key] -= LEARNINGRATE * grad[key]
 
         loss = network.loss(x_batch, t_batch)
 
