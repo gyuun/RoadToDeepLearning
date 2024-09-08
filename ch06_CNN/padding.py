6762e15b7fbdc329bd2632920522664b1b7f5597
gyuun <csegyuun@pusan.ac.kr>
2024-09-08 17:03:51 +0900

    padding with ndarray

--- ch06_CNN/padding.py
+++ ch06_CNN/padding.py
@@ -1,33 +1,10 @@
 """Handmade padding process module using list not ndarray"""
 import numpy as np
 
-def padding_2d(matrix : list, pad : int) -> list:
-    """2dim array padding function"""
-    padded_matrix = []
-    row = len(matrix)
-    col = len(matrix[0])
-    padded_row = row + 2*pad
-    padded_col = col + 2*pad
-    for i in range(padded_row):
-        new_col = [0 for i in range(padded_col)]
-        if pad <= i < pad + row:
-            padded_matrix.append(new_col[:pad] + matrix[i-pad] + new_col[:pad])
-        else:
-            padded_matrix.append(new_col)
-    return padded_matrix
-
 def padding(tensor : np.array, pad : int) -> np.array:
     """4dim array pading function"""
-    n, c = tensor.shape[:2]
-    padded_tensor = []
-    for i in range(n):
-        new_channel = []
-        for j in range(c):
-            print(list(tensor[i][j]))
-            new_matrix = padding_2d(tensor[i][j].tolist(), pad)
-            new_channel.append(new_matrix)
-        padded_tensor.append(new_channel)
-
-    result = np.array(padded_tensor)
+    n, c, h, w = tensor.shape
+    result = np.zeros((n, c, h + 2*pad, w + 2*pad))
+    result[:,:,pad:pad+h,pad:pad+w] = tensor
 
     return result
